5a981183c99beb7de46d85e5549ef50d38dc9280
LongVD <longvu.uet@gmail.com>
2020-06-26 20:03:20 +0700

    Update Webtruyen

--- webtruyen/page.js
+++ webtruyen/page.js
@@ -3,7 +3,7 @@ function execute(url) {
     var doc = Http.get(url).html();
     var totalPage = doc.select(".pagination a.last").attr("href").match(/\/(\d+)\//);
     if (totalPage) totalPage = totalPage[1];
-    else totalPage = 0;
+    else totalPage = 1;
 
     for (var i = 1; i <= totalPage; i++) {
         data.push(url + "/" + i);
