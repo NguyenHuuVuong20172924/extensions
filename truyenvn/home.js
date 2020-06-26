d83b64bd51b476f7d4ec0e7c27e0996cd1f7fc7b
LongVD <longvu.uet@gmail.com>
2020-06-26 19:43:04 +0700

    Update truyenvn

--- truyenvn/home.js
+++ truyenvn/home.js
@@ -1,5 +1,7 @@
 function execute() {
     return Response.success([
-        {title: "Mới cập nhật", input: "https://www.wattpad.com/api/v3/stories?filter=new&fields=stories(id,title,cover,description,url)", script: "gen.js"}
+        { title: "Mới cập nhật", input: "https://truyenvn.com/danh-sach-truyen", script: "gen.js" },
+        { title: "Truyện Hot", input: "https://truyenvn.com/truyen-hot", script: "gen.js" },
+        { title: "Hoàn thành", input: "https://truyenvn.com/truyen-hoan-thanh", script: "gen.js" }
     ]);
 }
