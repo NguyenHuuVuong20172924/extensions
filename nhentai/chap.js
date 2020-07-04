26e87cad901f5ed6cef5987cf7739a4d90a685d3
LongVD <longvu.uet@gmail.com>
2020-07-04 13:46:25 +0700

    Update nHentai

--- nhentai/chap.js
+++ nhentai/chap.js
@@ -1,7 +1,7 @@
 function execute(url) {
     var doc = Http.get(url).html();
     var el = doc.select("div#thumbnail-container div.thumb-container a img");
-    
+    el.select("noscript").remove();
     var data = [];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
