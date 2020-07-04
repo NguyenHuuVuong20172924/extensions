b28c9353acf3864960ffc7f9f5cc10c267698c3a
LongVD <longvu.uet@gmail.com>
2020-07-04 13:49:41 +0700

    Update nHentai

--- nhentai/chap.js
+++ nhentai/chap.js
@@ -1,11 +1,12 @@
 function execute(url) {
     var doc = Http.get(url).html();
-    var el = doc.select("div#thumbnail-container div.thumb-container a img");
+    var el = doc.select("div#thumbnail-container div.thumb-container a");
     el.select("noscript").remove();
     var data = [];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
-        data.push(e.attr("data-src").replace("t.nhentai.net", "i.nhentai.net").replace(/(\d+)t/, "$1"));
+       
+        data.push(e.select("img").attr("data-src").replace("t.nhentai.net", "i.nhentai.net").replace(/(\d+)t/, "$1"));
         
     }
     return Response.success(data);
