897d033f9a62ab30fbf38abf0da184c16719190d
LongVD <longvu.uet@gmail.com>
2020-07-09 23:08:12 +0700

    Add Sàn Truyện

--- webtruyen/search.js
+++ webtruyen/search.js
@@ -1,11 +1,10 @@
 function execute(key, page) {
     if (!page) page = '1';
-    const doc = Http.get("https://webtruyen.com/searching/" + key + "/lastupdate/all/all/" + page).html();
+    const doc = Http.get("https://webtruyen.com/searching/" + key.toLowerCase() + "/lastupdate/all/all/" + page).html();
 
     var next = doc.select(".pagination").select("li:has(.active) + li").select("a").text();
 
-    const el = doc.select(".list-content .list-row-img")
-
+    const el = doc.select(".list-content .list-row-img");
     const data = [];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
@@ -18,5 +17,5 @@ function execute(key, page) {
         })
     }
 
-    return Response.success(data, next)
+    return Response.success(data, next);
 }
