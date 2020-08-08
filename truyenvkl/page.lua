2c602142581b1e6fe10fdff3d8df58492749b9c5
LongVD <longvu.uet@gmail.com>
2020-08-08 08:57:48 +0700

    Update TruyenVKL

--- truyenvkl/page.lua
+++ truyenvkl/page.lua
@@ -3,10 +3,16 @@ local doc = http:get(url):html()
 local list = {}
 local pages = doc:select(".pagination li")
 local page = 1
-if pages:size() > 1 then
-    page = num:to_int(regexp:find(pages:get(pages:size() - 2):select("a"):attr("href"), "/(\\d+)"), 1)
+if pages:size() > 0 then
+    if pages:size() > 1 then
+        page = num:to_int(regexp:find(pages:get(pages:size() - 2):select("a"):attr("href"), "/(\\d+)"), 1)
+    end
+    for i = 1, page do
+        table.insert(list, url .. "/" .. i)
+    end
+elseif pages:size() == 0 then
+    local bookId = doc:select("#views"):attr("data-id")
+    table.insert(list, bookId)
 end
-for i = 1, page do
-    table.insert(list, url .. "/" .. i)
-end
+
 return response:success(list)
--- truyenvkl/toc.lua
+++ truyenvkl/toc.lua
@@ -1,8 +1,14 @@
 local url = ...
 local list = {}
-local doc = http:get(url):html()
-if doc ~= nil then
-    local el = doc:select(".listchap"):last():select("li a")
+local el
+if (text:contains(url, "truyenvkl.com")) then
+    local doc = http:get(url):html()
+    el = doc:select(".listchap"):last():select("li a")
+else
+    local doc = http:post("https://truyenvkl.com/wp-admin/admin-ajax.php"):params({ ["action"] = "all_chap", ["id"] = url }):html()
+    el = doc:select("a")
+end
+if el ~= nil then
     for i = 0, el:size() - 1 do
         local e = el:get(i)
         local chap = {}
