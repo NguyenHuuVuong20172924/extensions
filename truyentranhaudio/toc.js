3e80ec4b53b3e41cc4ac56c3d436fe6a5837ad28
LongVD <longvu.uet@gmail.com>
2020-08-25 21:36:56 +0700

    Add plugin

--- truyentranhaudio/toc.js
+++ truyentranhaudio/toc.js
@@ -1,7 +1,7 @@
 function execute(url) {
     var doc = Http.get(url).html();
 
-    var el = doc.select(".listing-chapters_wrap .li a")
+    var el = doc.select(".listing-chapters_wrap li a")
     const data = [];
     for (var i = el.size() - 1; i >= 0; i--) {
         var e = el.get(i);
