9ec4acf954d728d5ce015c59ecc577d4515962f6
LongVD <longvu.uet@gmail.com>
2020-09-18 20:28:28 +0700

    Update Beeng.net

--- beeng/home.js
+++ beeng/home.js
@@ -1,7 +1,7 @@
 function execute() {
     return Response.success([
-        {title: "Mới nhất", input: "https://beeng.net/get-data/moi-nhat", script: "top.js"},
-        {title: "Hot", input: "https://beeng.net/get-data/truyen-hot", script: "top.js"},
-        {title: "Xem nhiều", input: "https://beeng.net/get-data/xem-nhieu", script: "top.js"}
+        {title: "Mới nhất", input: "https://beeng.net/truyen-tranh-noi-bat/moi-nhat", script: "gen.js"},
+        {title: "Hot", input: "https://beeng.net/truyen-tranh-noi-bat/dang-hot", script: "gen.js"},
+        {title: "Xem nhiều", input: "https://beeng.net/truyen-tranh-noi-bat/xem-nhieu", script: "gen.js"}
     ]);
 }
