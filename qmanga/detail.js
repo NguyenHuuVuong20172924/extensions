04659582c8343af3a5479786bccb5126bba16361
LongVD <longvu.uet@gmail.com>
2020-10-22 00:34:41 +0700

    Update QManga

--- qmanga/detail.js
+++ qmanga/detail.js
@@ -2,7 +2,7 @@ function execute(url) {
     const doc = Http.get(url).html()
     return Response.success({
         name: doc.select("h1.title-commic-detail").first().text(),
-        cover: doc.select(".image-commic-detail img").first().attr("src"),
+        cover: doc.select(".image-commic-detail img").first().attr("data-src"),
         author: doc.select(".area-writer a").first().text(),
         description: doc.select(".desc-commic-detail").html(),
         detail: doc.select(".area-writer").html() + doc.select(".area-drawer").html() + doc.select(".status_commic").html(),
